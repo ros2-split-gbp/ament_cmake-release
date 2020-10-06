e444b0a7479a8db5cdfc39799abef66f3afeb4f3
ahcorde <ahcorde@gmail.com>
2020-10-06 19:22:39 +0200

    Rebase from 'upstream'

--- ament_cmake_google_benchmark/cmake/ament_add_google_benchmark_executable.cmake
+++ ament_cmake_google_benchmark/cmake/ament_add_google_benchmark_executable.cmake
@@ -1,4 +1,4 @@
-# Copyright 2014-2016 Open Source Robotics Foundation, Inc.
+# Copyright 2020 Open Source Robotics Foundation, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,49 +13,45 @@
 # limitations under the License.
 
 #
-# Add an executable using gtest.
+# Add an executable using google benchmark.
 #
-# Call add_executable(target ARGN) and link it against the gtest libraries.
+# Call add_executable(target ARGN) and link it against the google benchmark
+# libraries.
 # It does not register the executable as a test.
 #
-# If gtest is not available the specified target is not being created and
-# therefore the target existence should be checked before being used.
+# If google benchmark is not available the specified target is not being created
+# and therefore the target existence should be checked before being used.
 #
 # :param target: the target name which will also be used as the test name
 # :type target: string
 # :param ARGN: the list of source files
 # :type ARGN: list of strings
-# :param SKIP_LINKING_MAIN_LIBRARIES: if set skip linking against the gtest
-#   main libraries
+# :param SKIP_LINKING_MAIN_LIBRARIES: if set skip linking against the google
+#   benchmark main libraries
 # :type SKIP_LINKING_MAIN_LIBRARIES: option
 #
 # @public
 #
-macro(ament_add_gtest_executable target)
-  _ament_cmake_gtest_find_gtest()
-  if(GTEST_FOUND)
-    _ament_add_gtest_executable("${target}" ${ARGN})
+macro(ament_add_google_benchmark_executable target)
+  _ament_cmake_google_benchmark_find_benchmark()
+  if(benchmark_FOUND)
+    _ament_add_google_benchmark_executable("${target}" ${ARGN})
   endif()
 endmacro()
 
-function(_ament_add_gtest_executable target)
+function(_ament_add_google_benchmark_executable target)
   cmake_parse_arguments(ARG "SKIP_LINKING_MAIN_LIBRARIES" "" "" ${ARGN})
   if(NOT ARG_UNPARSED_ARGUMENTS)
     message(FATAL_ERROR
-      "ament_add_gtest_executable() must be invoked with at least one source file")
+      "ament_add_google_benchmark_executable() must be invoked with at least "
+      "one source file")
   endif()
 
   # should be EXCLUDE_FROM_ALL if it would be possible
   # to add this target as a dependency to the "test" target
   add_executable("${target}" ${ARG_UNPARSED_ARGUMENTS})
-  target_include_directories("${target}" BEFORE PUBLIC "${GTEST_INCLUDE_DIRS}")
   if(NOT ARG_SKIP_LINKING_MAIN_LIBRARIES)
-    target_link_libraries("${target}" ${GTEST_MAIN_LIBRARIES})
+    target_link_libraries("${target}" benchmark::benchmark_main)
   endif()
-  target_link_libraries("${target}" ${GTEST_LIBRARIES})
-  if(NOT WIN32)
-    set(THREADS_PREFER_PTHREAD_FLAG ON)
-    find_package(Threads REQUIRED)
-    target_link_libraries("${target}" Threads::Threads)
-  endif()
+  target_link_libraries("${target}" benchmark::benchmark)
 endfunction()
