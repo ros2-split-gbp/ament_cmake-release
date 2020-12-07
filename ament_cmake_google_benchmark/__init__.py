cbbe4107c40c54a8669b4fcc089457b295b531a6
Chris Lalancette <clalancette@openrobotics.org>
2020-12-07 23:37:53 +0000

    Rebase from 'debian/rolling/ament_cmake_google_benchmark'

--- ament_cmake_google_benchmark/__init__.py
+++ ament_cmake_google_benchmark/__init__.py
@@ -74,7 +74,9 @@ def main(argv=sys.argv[1:]):
     args = parser.parse_args(argv)
     args.command = command
 
+    print('Executing benchmark test command: %s\n\n' % ' '.join(args.command))
     res = subprocess.run(args.command)
+    print('\n\nTest command returned result status %d' % res.returncode)
 
     try:
         with open(args.result_file_in, 'r') as in_file:
@@ -94,7 +96,17 @@ def main(argv=sys.argv[1:]):
         open(args.result_file_out, 'w').close()
         return res.returncode
 
-    in_data = json.loads(in_text)
+    try:
+        in_data = json.loads(in_text)
+    except json.decoder.JSONDecodeError as e:
+        print(
+            'Failure parsing performance results file at: %s' % args.result_file_in,
+            file=sys.stderr)
+        print(e)
+        if res.returncode == 0:
+            res.returncode = 1
+        return res.returncode
+
     overlay_data = None
     if args.result_file_overlay:
         with open(args.result_file_overlay, 'r') as overlay_file:
@@ -103,6 +115,10 @@ def main(argv=sys.argv[1:]):
         in_data, overlay_data, args.package_name)
     with open(args.result_file_out, 'w') as out_file:
         json.dump(out_data, out_file)
+
+    if res.returncode == 0 and any(
+            b.get('error_occurred') for b in in_data.get('benchmarks', [])):
+        res.returncode = 1
 
     return res.returncode
 
@@ -148,7 +164,12 @@ def convert_aggregate_benchmark(in_data):
         },
     }
 
-    out_data.update(convert_extra_metrics(in_data, common_aggregate_test_properties))
+    value_override = None
+    if in_data.get('error_occurred', False):
+        value_override = 'failure'
+
+    out_data.update(convert_extra_metrics(
+        in_data, common_aggregate_test_properties, value_override))
 
     return out_data
 
@@ -164,25 +185,37 @@ def convert_iteration_benchmark(in_data):
             },
         },
         'cpu_time': {
-            'dblValue': in_data['cpu_time'],
             'unit': in_data['time_unit'],
         },
         'real_time': {
-            'dblValue': in_data['real_time'],
             'unit': in_data['time_unit'],
         },
     }
 
-    out_data.update(convert_extra_metrics(in_data, common_iteration_test_properties))
+    value_override = None
+    if in_data.get('error_occurred', False):
+        value_override = 'failure'
+        out_data['cpu_time']['value'] = value_override
+        out_data['real_time']['value'] = value_override
+    else:
+        out_data['cpu_time']['dblValue'] = in_data['cpu_time']
+        out_data['real_time']['dblValue'] = in_data['real_time']
+
+    out_data.update(convert_extra_metrics(
+        in_data, common_iteration_test_properties, value_override))
 
     return out_data
 
 
-def convert_extra_metrics(in_data, properties_to_ignore):
+def convert_extra_metrics(in_data, properties_to_ignore, value_override=None):
     for k, v in in_data.items():
         if k in properties_to_ignore:
             continue
-        if isinstance(v, bool):
+        if value_override:
+            yield k, {
+                'value': value_override,
+            }
+        elif isinstance(v, bool):
             yield k, {
                 'boolValue': 'true' if v else 'false',
             }
